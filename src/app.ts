3fad7db828209e37ad669a80af440f5c335cdcf7
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-11-25 11:50:39 +0200

    Changed data path

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use('/', router)
 app.use(express.static(path.join(__dirname, '../public')))
 
 // Add "database"
-const dataPath = "../data.json"; 
+const dataPath = "./data.json"; 
 const database: Database = new Database(dataPath); 
 
 export default app; 
